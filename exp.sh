f73000f81d0b5235f3c90668a54bab79056a7c97
lihe50hz <lihe50hz@gmail.com>
2024-08-24 19:50:08 +0800

    test

---
+++ exp.sh
@@ -0,0 +1,15 @@
+KARY=4
+NDIM=3
+# ncores = kary^ndim
+N_CORES=$(( $KARY ** $NDIM ))
+echo "kary = $KARY, ndim = $NDIM, ncores = $N_CORES"
+
+./build/NULL/gem5.opt \
+configs/example/garnet_synth_traffic.py \
+--network=garnet --num-cpus=$N_CORES --num-dirs=$N_CORES \
+--topology=Torus --kary=$KARY --ndim=$NDIM \
+--routing-algorithm=2 \
+--inj-vnet=0 --synthetic=uniform_random \
+--injectionrate=0.01 \
+--sim-cycles=1000000000 \
+--garnet-deadlock-threshold 50000
